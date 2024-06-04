73f53d82e722f91aac6dbda387d043c51ed7add4
leoaraiza <139597481+leoaraiza@users.noreply.github.com>
2024-06-03 19:04:59 -0600

    Delete PortalWeb directory

--- PortalWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PortalWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
